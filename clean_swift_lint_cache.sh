3d59252100cdef6b577665eddb8a0a88ad8deccd
Kazuki Kubo <kz@assaulter.org>
2020-09-29 15:27:16 +0900

    update

---
+++ clean_swift_lint_cache.sh
@@ -0,0 +1,2 @@
+rm -rf ~/Library/Caches/SwiftLint
+
---
+++ clean_xcode_cache.sh
@@ -0,0 +1,4 @@
+rm -rf ~/Library/Caches/com.apple.dt.Xcode/
+rm -rf ~/Library/Developer/Xcode/DerivedData/
+rm -rf ~/Library/Developer/CoreSimulator/Caches/dyld/
+rm -rf ~/Library/Developer/Xcode/Archives/
