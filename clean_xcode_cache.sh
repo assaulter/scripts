468dab04ea6b3fdb175e2f32c77905536ae5ffe3
Kazuki Kubo <kz@assaulter.org>
2020-12-21 12:08:03 +0900

    update

--- clean_xcode_cache.sh
+++ clean_xcode_cache.sh
@@ -1,4 +1,8 @@
 rm -rf ~/Library/Caches/com.apple.dt.Xcode/
 rm -rf ~/Library/Developer/Xcode/DerivedData/
-rm -rf ~/Library/Developer/CoreSimulator/Caches/dyld/
 rm -rf ~/Library/Developer/Xcode/Archives/
+rm -rf ~/Library/Developer/Xcode/Products/
+rm -rf ~/Library/Developer/CoreSimulator/Caches/dyld/
+
+# シミュレータを初期化する
+# xcrun simctl erase all
