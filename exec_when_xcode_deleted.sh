70453d6b99f689fe7523100ec7c0cddcb1bd34f2
Kazuki Kubo <kz@assaulter.org>
2021-06-09 13:35:55 +0900

    update

---
+++ exec_when_xcode_deleted.sh
@@ -0,0 +1,5 @@
+rm -rf ~/Library/Developer/Xcode/iOS\ DeviceSupport/
+rm -rf ~/Library/Developer/Xcode/watchOS\ DeviceSupport/
+rm -rf ~/Library/Developer/Xcode/iOS\ Device\ Logs/
+rm -rf ~/Library/Developer/Xcode/UserData/
+rm -rf ~/Library/Developer/Xcode/DocumentationCache/
